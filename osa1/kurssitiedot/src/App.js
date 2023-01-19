c08638d0c1abc2397c13d2cf35300249e2d0e304
ftonvi <onni.vitikainen@tuni.fi>
2023-01-19 17:02:51 +0200

    kurssitiedot step2

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -5,23 +5,31 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return(
     <>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <p>{props.part} {props.exercises}</p>
     </>
-  
+  )
+}
+
+
+const Content = (props) => {
+  return(
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
   )
 }
 
 const Total = (props) => {
-return(
-  <>
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-  </>
-)
+  return(
+    <>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </>
+  )
 }
 
 const App = () => {
