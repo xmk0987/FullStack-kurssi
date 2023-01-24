11fcb2fad0689b7f7534e40aa345acd1adb68b77
ftonvi <onni.vitikainen@tuni.fi>
2023-01-24 19:42:22 +0200

    kurssitiedot step6

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,60 @@
+const Header = (props) => {
+  return(
+    <h1>{props.course}</h1>
+    
+  )
+}
+
+const Content = (props) => {
+  return(
+    props.parts.map(part => <p key={part.name}>{part.name} {part.exercises}</p>)
+  )
+}
+ 
+const Total = (props) => {
+  return(
+    <>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    </>
+  )
+}
+
+//<Total parts = {props.course.parts}/>
+const Course = (props) =>{
+  return (
+    <div>
+      <Header course={props.course.name} />
+      <Content parts = {props.course.parts}/>
+      
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
