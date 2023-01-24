7969d82bd59f2443f6ac340d617eafdf8bfd5465
ftonvi <onni.vitikainen@tuni.fi>
2023-01-24 02:24:28 +0200

    unicafe step5

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -6,6 +6,10 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
+const StatisticLine = ({text, value}) => (
+  <p>{text} {value}</p>
+)
+
 const Statistics = (props) => {
   var all = props.good + props.neutral + props.bad
   if(all == 0){
@@ -14,14 +18,14 @@ const Statistics = (props) => {
     )
   }
   return(
-  <>
-    <p>good {props.good}</p>
-    <p>neutral {props.neutral}</p>
-    <p>bad {props.bad}</p>
-    <p>all {all}</p>
-    <p>average {(props.good-props.bad)/(all)}</p>
-    <p>positive {props.good/all*100} %</p>
-  </>
+  <div>
+    <StatisticLine text = "good" value={props.good}/>
+    <StatisticLine text = "neutral" value={props.neutral}/>
+    <StatisticLine text = "bad" value={props.bad}/>
+    <StatisticLine text = "all" value={all}/>
+    <StatisticLine text = "average" value={(props.good-props.bad)/(all)}/>
+    <StatisticLine text = "positive" value={props.good/all*100 + ' %'}/>
+  </div>
   )
 
 }
