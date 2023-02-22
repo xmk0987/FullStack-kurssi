142dda50cc88b3d6631c09adb207e19e76502c04
ftonvi <onni.vitikainen@tuni.fi>
2023-02-23 00:56:24 +0200

    minor changes

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -18,20 +18,16 @@ let persons = [
         id:3,
         name: "Dan Abramov",
         number: "12-43-234345"
-    },
-    {
-        id:4, 
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
     }
 ]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
-
-
-
 
 
 const PORT = 3001
